d8508d8f72723edba67b0aa7263268ac45539036
Kaviarasi90 <101966808+Kaviarasi90@users.noreply.github.com>
2022-04-19 00:18:24 +0530

    Add files via upload

---
+++ FindIntersection.java
@@ -0,0 +1,29 @@
+package week1.day2;
+
+import java.util.ArrayList;
+
+public class FindIntersection {
+	
+
+	public static void main(String[] args) {
+		int[] arr = {3,2,11,4,6,7};
+		int[] arr1= {1,2,8,4,9,7};
+		FindIntersection same = new FindIntersection();
+		
+	}
+			public ArrayList<Integer> FindCommonElement(int[] arr,int[] arr1) {
+				ArrayList<Integer> Common = new ArrayList<Integer>();
+				for (int i=0;i<arr.length;i++)
+				{
+					for (int j=0;j<arr1.length;j++)
+					{
+						if(arr[i]==arr1[j]){
+							
+							{
+	                        	Common.add(arr[i]);
+	                        }}}}
+	     	       return Common;	    
+	        }}
+
+				
+			
---
+++ MissingElementInAnArray.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+import java.util.Arrays;
+
+
+public class MissingElementInAnArray {
+	public static void main(String[] args) {
+		int [] arr = {1,2,3,4,7,6,8};
+		Arrays.sort(arr);
+		int k=0,i=1;
+		for (i = arr[0]; i<=arr.length;i++)
+			
+			System.out.println(i);	
+		k = k+arr[i];
+	}
+		
+	}
+
+
+
